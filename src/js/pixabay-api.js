d6cae4475703d6675084c2ad9f184f24a61abfd9
Nadiia1701 <146233511+Nadiia1701@users.noreply.github.com>
2024-03-05 05:36:16 -0500

    Summary1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,44 +1,56 @@
+
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+import axios from 'axios';
 
 const API_KEY = '42569915-bcd29008899db620988a57306';
 const BASE_URL = 'https://pixabay.com/api/';
 const loader = document.querySelector('.loader');
 
-export function fetchData(searchQuery) {
+let perPage = 15;
+
+export async function fetchData(searchQuery, currentPage) {
   const params = new URLSearchParams({
     key: API_KEY,
     q: searchQuery,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    page: currentPage,
+    per_page: perPage,
   });
 
   loader.style.display = 'block';
 
-  return fetch(`${BASE_URL}?${params}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not OK!');
-      }
-      return response.json();
-    })
-    .then(data => {
-      loader.style.display = 'none';
+  try {
+    const response = await axios.get(`${BASE_URL}?${params}`);
 
-      if (data.hits.length === 0) {
-        iziToast.error({
-          fontSize: 'large',
-          close: false,
-          position: 'topRight',
-          messageColor: 'white',
-          timeout: 2000,
-          backgroundColor: 'red',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-      }
-      return data;
-    })
-    .catch(error => console.error('Error fetching data:', error));
+    if (!response.status === 200) {
+      throw new Error('Network response was not OK!');
+    }
+
+    const data = response.data;
+
+    loader.style.display = 'none';
+
+    if (data.hits.length === 0) {
+      iziToast.error({
+        fontSize: 'large',
+        close: false,
+        position: 'topRight',
+        messageColor: 'white',
+        timeout: 2000,
+        backgroundColor: 'red',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+    }
+
+    return data;
+
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 import { fetchData } from "./js/pixabay-api.js";
 import { renderMurcup } from "./js/render-functions.js";
 
@@ -11,20 +14,69 @@ const lightbox = new SimpleLightbox(".gallery-link", {
 
 const form = document.querySelector("#searchForm");
 const container = document.querySelector(".gallery");
+const loadMoreBtn = document.querySelector(".load-more-btn");
 let searchQuery = "";
+let currentPage = 1;
+let totalHits = 0;
+
+loadMoreBtn.style.display = "none";
 
 form.addEventListener("submit", onSubmit);
+loadMoreBtn.addEventListener("click", onLoadMore);
 
 function onSubmit(event) {
   event.preventDefault();
   container.innerHTML = "";
-
+  currentPage = 1;
   searchQuery = form.elements.searchQuery.value.trim();
+  fetchImages();
+}
 
-  fetchData(searchQuery).then(data => {
-    const murcup = renderMurcup(data);
-    container.insertAdjacentHTML('beforeend', murcup);
-    lightbox.refresh();
-  })
+function onLoadMore() {
+  currentPage++;
+  fetchImages();
+}
+
+function fetchImages() {
+  fetchData(searchQuery, currentPage)
+    .then(data => {
+      totalHits = data.totalHits;
+      const murcup = renderMurcup(data);
+      container.insertAdjacentHTML('beforeend', murcup);
+      lightbox.refresh();
+      handleLoadMoreButton(data);
+    })
     .catch((error) => console.error("Error fetching data:", error));
 }
+
+function handleLoadMoreButton(data) {
+  if (container.children.length < totalHits) {
+    loadMoreBtn.style.display = "block";
+  } else {
+    const currentHits = currentPage * 15;
+    if (currentHits >= totalHits) {
+      loadMoreBtn.style.display = "none";
+      iziToast.info({
+        title: 'Info',
+        message: `We're sorry, but you've reached the end of search results.`,
+        backgroundColor: '#4CAF50',
+        messageColor: '#fff',
+        titleColor: '#fff',
+        progressBarColor: '#4CAF50',
+        position: 'topRight',
+      });
+    } else {
+      renderMurcup(data); 
+    }
+  }
+}
+
+function smoothScroll() {
+  const itemHeight = container.firstElementChild.getBoundingClientRect().height;
+  window.scrollBy({
+    top: 3 * itemHeight,
+    behavior: "smooth",
+  });
+}
+
+container.addEventListener("scroll", smoothScroll);
